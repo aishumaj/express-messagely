06fbec8ba617da2d7242a3e788c279c432ec712b
Aishwarya Majumdar <aishu.maj1996@gmail.com>
2022-08-22 12:40:51 -0700

    user model methods updates

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const db = require("../db");
+const { NotFoundError} = require("../expressError");
+
 /** User of the site. */
 
 class User {
@@ -9,22 +14,71 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    //hash the pw
+    let hashedPw = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    
+    //insert into users table
+    const results = await db.query(
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          last_name,
+                          phone,
+                          join_at,
+                          last_login_at)
+        VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+        RETURNING username, password, first_name, last_name, phone`,
+        [username, hashedPw, first_name, last_name, phone],
+    );
+    
+    return results.rows[0]; 
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    //get the pw
+    const result = await db.query(
+      `SELECT password FROM users WHERE username = $1`,
+      [username],
+    );
+    
+    const user = result.rows[0];
+    
+    //bcrypt.compare
+    if(user){
+      return await bcrypt.compare(password, user.password) === true;
+    }
+    return false;
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+      SET last_login_at=current_timestamp
+      WHERE username = $1
+      RETURNING username`,
+      [username],
+    );
+    const user = result.rows[0];
+    
+    if(!user){
+      throw new NotFoundError(message = `${username} is not a valid username.`)
+    }
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name
+      FROM users`
+    )
+    
+    return results.rows;
   }
 
   /** Get: get user by username
@@ -37,6 +91,23 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const results = await db.query(
+      `SELECT username,
+              first_name,
+              last_name,
+              phone,
+              join_at,
+              last_login_at
+      FROM users
+      WHERE username = $1`,
+      [username]
+    );
+    const user = results.rows[0];
+    
+    if(!user){
+      throw new NotFoundError(message = `${username} is not a valid username.`)
+    }
+    return user;
   }
 
   /** Return messages from this user.
@@ -45,9 +116,44 @@ class User {
    *
    * where to_user is
    *   {username, first_name, last_name, phone}
+   * 
+   * result:
+   * [{id, to_user:{username, first_name, last_name, phone}, body, sent_at, read_at}]
    */
 
   static async messagesFrom(username) {
+    const results = await db.query(
+      `SELECT m.id,
+              m.to_username,
+              u.first_name,
+              u.last_name,
+              u.phone,
+              m.body,
+              m.sent_at,
+              m.read_at
+      FROM messages AS m
+        JOIN users AS u
+          ON m.to_username = u.username
+      WHERE m.from_username = $1`,
+      [username]
+    );
+    
+    const messagesFromUser = results.rows;
+    
+    //Question: Is there a better way to do this that isn't brute force.
+    return messagesFromUser.map(m => ({
+      id: m.id,
+      to_user: {
+        username: m.to_username,
+        first_name: m.first_name,
+        last_name: m.last_name,
+        phone: m.phone
+      },
+      body: m.body,
+      sent_at: m.sent_at,
+      read_at: m.read_at,
+    }));
+    
   }
 
   /** Return messages to this user.
